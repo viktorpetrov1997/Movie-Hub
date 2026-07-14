b7cf8c6d6a98bd87575588d4b8f71474cbdc466d
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2026-07-14 03:51:29 +0300

    refactor: move authentication endpoints to AuthController

--- src/main/java/app/home/IndexController.java
+++ src/main/java/app/home/IndexController.java
@@ -1,92 +1,15 @@
 package app.home;
 
-import app.user.UserService;
-import app.user.dto.UserDto;
-import app.user.dto.UserLoginRequest;
-import app.user.dto.UserRegisterRequest;
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController
 {
-    private final UserService userService;
-
-    public IndexController(UserService userService)
-    {
-        this.userService = userService;
-    }
-
     @GetMapping("/")
     public ModelAndView index()
     {
         return new ModelAndView("index");
-    }
-
-    @GetMapping("/register")
-    public ModelAndView getRegisterPage()
-    {
-        UserRegisterRequest userRegisterRequest = UserRegisterRequest.builder().build();
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("register");
-        modelAndView.addObject("userRegisterRequest", userRegisterRequest);
-
-        return modelAndView;
-    }
-
-    @PostMapping("/register")
-    public ModelAndView registerUser(@Valid UserRegisterRequest userRegisterRequest,
-                                     BindingResult bindingResult)
-    {
-        if(bindingResult.hasErrors())
-        {
-            ModelAndView modelAndView = new ModelAndView();
-            modelAndView.setViewName("register");
-            modelAndView.addObject("userRegisterRequest", userRegisterRequest);
-            return modelAndView;
-        }
-
-        userService.register(userRegisterRequest);
-
-        return new ModelAndView("redirect:/login");
-    }
-
-    @GetMapping("/login")
-    public ModelAndView getLoginPage()
-    {
-        UserLoginRequest userLoginRequest = UserLoginRequest.builder().build();
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("login");
-        modelAndView.addObject("userLoginRequest", userLoginRequest);
-
-        return modelAndView;
-    }
-
-    @PostMapping("/login")
-    public ModelAndView login(@Valid UserLoginRequest userLoginRequest,
-                              BindingResult bindingResult,
-                              HttpSession httpSession
-    )
-    {
-        if(bindingResult.hasErrors())
-        {
-            ModelAndView modelAndView = new ModelAndView();
-            modelAndView.setViewName("login");
-            modelAndView.addObject("userLoginRequest", userLoginRequest);
-            return modelAndView;
-        }
-
-        UserDto user = userService.login(userLoginRequest);
-
-        httpSession.setAttribute("user_id", user.getId());
-
-        return new ModelAndView("redirect:/");
     }
 }
